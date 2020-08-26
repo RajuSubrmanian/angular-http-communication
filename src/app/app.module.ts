0c661d925dffb2411a2aefc5d1914c3ff3c30e74
S <raju.a.raju@capgemini.com>
2020-08-26 14:00:05 +0530

    created menus and components for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AllBooksComponent } from './books/components/all-books/all-books.component';
+import { TitleComponent } from './books/components/title/title.component';
+import { DateComponent } from './books/components/date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AllBooksComponent,
+    TitleComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule,
